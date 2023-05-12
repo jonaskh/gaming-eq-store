341a76e50e75c46a1081042e71b94bf7c1721671
Daniel Lundquist <danilu@stud.ntnu.no>
2023-05-12 19:08:13 +0200

    Chaning url for docker server setup

--- api/src/main/java/no/ntnu/idata/gamingeqstore/Controllers/ProductController.java
+++ api/src/main/java/no/ntnu/idata/gamingeqstore/Controllers/ProductController.java
@@ -3,10 +3,7 @@ package no.ntnu.idata.gamingeqstore.Controllers;
 import no.ntnu.idata.gamingeqstore.Entities.Product;
 import no.ntnu.idata.gamingeqstore.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +13,7 @@ import java.util.stream.StreamSupport;
 
 @CrossOrigin(origins = "http://backend:3000")
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
