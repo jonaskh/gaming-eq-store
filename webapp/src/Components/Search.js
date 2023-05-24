b1e42b92253da7d3b0e278da135f5e3c7cf6e939
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-05-24 12:58:37 +0200

    Added selectable categories to shop page

--- webapp/src/Components/Search.js
+++ webapp/src/Components/Search.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../css/Search.css'; // New line to import Search CSS
 
-function Search({ value, onChange }) {
+function Search({ value, onChange, placeholder }) { // Add placeholder prop
   return (
-    <input type="text" placeholder="Search products" value={value} onChange={onChange} />
+      <input
+          className="search-input"
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+      />
   );
 }
 
