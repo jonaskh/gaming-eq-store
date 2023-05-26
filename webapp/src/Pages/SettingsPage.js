f919d7f218efddc073ac24919ff0686c5c235cf9
Daniel Lundquist <danilu@stud.ntnu.no>
2023-05-26 13:40:19 +0200

    updates to docker-compose and nginx settings

--- webapp/src/Pages/SettingsPage.js
+++ webapp/src/Pages/SettingsPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import jwt_decode from 'jwt-decode';
 import '../css/SettingsPage.css';
