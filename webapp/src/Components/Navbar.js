f705dbc7d52bad5fa4e57610df4214af9ef35e72
Daniel Lundquist <danilu@stud.ntnu.no>
2023-05-26 23:22:10 +0200

    new logo with redirect

--- webapp/src/Components/Navbar.js
+++ webapp/src/Components/Navbar.js
@@ -1,36 +1,34 @@
-import React, {useState} from 'react';
-import DropdownMenu from './DropdownMenu';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
-import Logo from './Logo';
-import {Link} from 'react-router-dom';
+
 
 function Navbar() {
-
-
-    return (
-        <nav className="navbar">
-            <li> <Link to="/">Cyberpunk Gaming Gear</Link>
-            <div className="navBarTitle">Cyberpunk Gaming Gear</div>
-            </li>
-            <ul className="menu">
-                <li><Link to="/">Home</Link></li>
-                <li>
-                    <Link to="/shop">Shop</Link>
-                </li>
-                <li>
-                    <a href="/cart">
-                        <img src={`${process.env.PUBLIC_URL}/icons8-shopping-cart-30.png`} alt="Cart"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="/settings">
-                        <img src={`${process.env.PUBLIC_URL}/icons8-male-user-24.png`} alt="Profile"/>
-
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo-link">
+        <img className="logo" src={process.env.PUBLIC_URL + '/logo-no-background.png'} alt="Logo" style={{ width: '150px', height: 'auto' }} />
+      </Link>
+      <ul className="menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <a href="/cart">
+            <img src={`${process.env.PUBLIC_URL}/icons8-shopping-cart-30.png`} alt="Cart" />
+          </a>
+        </li>
+        <li>
+          <a href="/settings">
+            <img src={`${process.env.PUBLIC_URL}/icons8-male-user-24.png`} alt="Profile" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
