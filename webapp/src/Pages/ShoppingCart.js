a8a91452baac0e82ce45d361687dcbc02079ae3d
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-05-28 15:27:04 +0200

    Checkout button disabled when no items in cart and after checking out

--- webapp/src/Pages/ShoppingCart.js
+++ webapp/src/Pages/ShoppingCart.js
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from "../Components/Footer";
 import APIService from "../Services/APIService";
 import CartItem from "../Components/CartItem";
 import "../css/Cart.css"
 import jwt_decode from "jwt-decode";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 function ShoppingCart() {
     const navigate = useNavigate();
@@ -14,9 +13,9 @@ function ShoppingCart() {
     const [error, setError] = useState(null);
     const [totalCost, setTotalCost] = useState(0);
     const token = localStorage.getItem('jwt');
+    let checkoutButtonTempDisabled = false;
 
     useEffect(() => {
-
         if (!token) {
             navigate('/login');
         } else {
@@ -43,11 +42,12 @@ function ShoppingCart() {
     }, [navigate]);
 
     if (error) {
-        return <div className="error">{error}</div>
+        return <div className="error">{error}</div>;
     }
+
     const handleQuantityChange = (id, newQuantity) => {
-        const updatedProducts = products.map((product) =>
-            product.id === id ? {...product, productAmount: newQuantity} : product
+        const updatedProducts = products.map(product =>
+            product.id === id ? { ...product, productAmount: newQuantity } : product
         );
         setProducts(updatedProducts);
 
@@ -61,50 +61,57 @@ function ShoppingCart() {
         try {
             const decodedToken = jwt_decode(token);
             const email = decodedToken.sub;
-            APIService.addOrderFromCartItems(email).then(response => {
-                APIService.deleteAllItemsInCart(email).then(e => {
-                        window.location.reload();
-                    }
-                )}
-            );
+            checkoutButtonTempDisabled = true;
+            APIService.addOrderFromCartItems(email)
+                .then(response => {
+                    APIService.deleteAllItemsInCart(email)
+                        .then(() => {
+                            window.location.reload();
+                            checkoutButtonTempDisabled = false;
+                        })
+                        .catch(error => console.log(error));
+                })
+                .catch(error => console.log(error));
         } catch (err) {
             setError('Invalid token');
             localStorage.removeItem('jwt');
         }
     }
 
-
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="cart-section">
                 <div className="cart-container">
                     <h2 className="section-title">Your Cart</h2>
                     <section className="items">
-                        {products.map((product) => (
-                            <CartItem
-                                key={product.id}
-                                id={product.id}
-                                image={product.productImage}
-                                title={product.productName}
-                                price={product.price}
-                                itemQuantity={product.productAmount}
-                                onQuantityChange={handleQuantityChange}
-
-                            />
-                        ))}
+                        {products.length === 0 ? (
+                            <p className="empty-cart">Your cart is empty</p>
+                        ) : (
+                            products.map(product => (
+                                <CartItem
+                                    key={product.id}
+                                    id={product.id}
+                                    image={product.productImage}
+                                    title={product.productName}
+                                    price={product.price}
+                                    itemQuantity={product.productAmount}
+                                    onQuantityChange={handleQuantityChange}
+                                />
+                            ))
+                        )}
                     </section>
                 </div>
 
-                <h2>You fucker</h2>
                 <div className="cart-info">
                     Grand total: {totalCost}
                 </div>
-                <button onClick={handleCheckout}>Checkout</button>
-
+                <button disabled={products.length === 0 || checkoutButtonTempDisabled} onClick={handleCheckout}>
+                    Checkout
+                </button>
             </div>
 
-            <Footer/>
+            <Footer />
         </div>
     );
 }
