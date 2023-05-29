499cd6309b4ffd63e00f0db49bf03921677c9046
Stian B Kaspersen <stianbka@stud.ntnu.no>
2023-05-29 16:23:09 +0200

    Changed index.js so it fits with react 18 design

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+
