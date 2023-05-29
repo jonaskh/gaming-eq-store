e46c0ecf192688661f641253e12ade370bf655ef
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-05-29 15:38:41 +0200

    Slider to homepage

--- webapp/src/Pages/Home.js
+++ webapp/src/Pages/Home.js
@@ -6,6 +6,9 @@ import Testimonial from '../Components/Testimonial';
 import Hero from '../Components/Hero';
 import APIService from "../Services/APIService";
 import Footer from "../Components/Footer";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 function Home() {
@@ -17,60 +20,87 @@ function Home() {
         });
     }, []);
 
-  const testimonials = [
-    {
-      name: 'Joe',
-      age: 17,
-      text: "I recently purchased a headset from Cyberpunk Gaming Gear, and it was an awesome experience. The headset is super comfortable and has amazing sound quality. It's really helped me up my game and I'm so glad I made this purchase!",
-    },
-    {
-      name: 'Alex',
-      age: 22,
-      text: "As a serious e-sports player, I need gear that can keep up with me and my intense gaming sessions. Cyberpunk Gaming Gear has been a lifesaver! The products I've purchased are comfortable, durable, and perform amazingly well. I'm so impressed with the quality and value of the products from this company!",
-    },
-    {
-      name: 'Sarah',
-      age: 37,
-      text: "As a mother of two young gamers, I'm always looking for the best gaming gear for my kids. Cyberpunk Gaming Gear has been a great resource for me as they offer high-quality products at great prices. I know that my kids will be safe and comfortable while gaming with their new gear, and I'm so grateful to have found such a dependable company!",
-    },
-  ];
+    const testimonials = [
+        {
+            name: 'Joe',
+            age: 17,
+            text: "I recently purchased a headset from Cyberpunk Gaming Gear, and it was an awesome experience. The headset is super comfortable and has amazing sound quality. It's really helped me up my game and I'm so glad I made this purchase!",
+        },
+        {
+            name: 'Alex',
+            age: 22,
+            text: "As a serious e-sports player, I need gear that can keep up with me and my intense gaming sessions. Cyberpunk Gaming Gear has been a lifesaver! The products I've purchased are comfortable, durable, and perform amazingly well. I'm so impressed with the quality and value of the products from this company!",
+        },
+        {
+            name: 'Sarah',
+            age: 37,
+            text: "As a mother of two young gamers, I'm always looking for the best gaming gear for my kids. Cyberpunk Gaming Gear has been a great resource for me as they offer high-quality products at great prices. I know that my kids will be safe and comfortable while gaming with their new gear, and I'm so grateful to have found such a dependable company!",
+        },
+    ];
 
-  return (
-    <div className="Home">
-      <Navbar />
-      <section className="hero-section">
-        <Hero
-          title="Welcome to Cyberpunk Gaming Gear"
-          subtitle="Upgrade your game with cutting-edge gear from Cyberpunk Gaming Gear!"
-          buttonText="Shop now"
-        />
-      </section>
+    const discounts = [
+        {text: '30% off on all controllers', buttonText: 'Shop Now'},
+        {text: 'The finest products for your gaming setup', buttonText: 'Shop Now'},
+        //{image: '/discounts/img3.jpg', text: 'Exclusive deals for new customers', buttonText: 'Join Us'},
+    ];
 
-      <h2 className="section-title">Featured Products</h2>
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
 
-      <section className="products">
+    return (
+        <div className="Home">
+            <Navbar/>
+            <section className="hero-section">
+                <Hero
+                    title="Welcome to Cyberpunk Gaming Gear"
+                    subtitle="Upgrade your game with cutting-edge gear from Cyberpunk Gaming Gear!"
+                    buttonText="Shop now"
+                />
+            </section>
 
-        {products.map((product) => (
-          <ProductCard
-            key={product.product_id}
-            id={product.product_id}
-            image={product.productImage}
-            title={product.productName}
-            price={product.price}
-          />
-        ))}
-      </section>
+            <h2 className="section-title">Featured Products</h2>
 
-      <h2 className="section-title">Testimonials</h2>
+            <section className="products">
 
-      <section className="testimonials">
-        {testimonials.map((testimonial, index) => (
-          <Testimonial key={index} name={testimonial.name} age={testimonial.age} text={testimonial.text} />
-        ))}
-      </section>
-        <Footer/>
-    </div>
-  );
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.product_id}
+                        id={product.product_id}
+                        image={product.productImage}
+                        title={product.productName}
+                        price={product.price}
+                    />
+                ))}
+            </section>
+
+            <section className="discounts">
+                <Slider {...sliderSettings}>
+                    {discounts.map((discount, index) => (
+                        <div key={index} className={`discount-slide discount-slide-${index}`}>
+                            <div className="slide-section">
+                                <h1 className="slider-text">{discount.text}</h1>
+                                <button>{discount.buttonText}</button>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+            </section>
+
+            <h2 className="section-title">Testimonials</h2>
+
+            <section className="testimonials">
+                {testimonials.map((testimonial, index) => (
+                    <Testimonial key={index} name={testimonial.name} age={testimonial.age} text={testimonial.text}/>
+                ))}
+            </section>
+            <Footer/>
+        </div>
+    );
 }
 
 export default Home;
